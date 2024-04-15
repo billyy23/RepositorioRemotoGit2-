316897caccaaffce0142f77be5403922ec63149a
Bilal <Bilal@AX15>
2024-04-15 18:18:31 +0200

    commit de rama1

---
+++ Proyecto2/src/paqueteRama1/rama1.java
@@ -0,0 +1,11 @@
+package paqueteRama1;
+
+public class rama1 {
+
+	public static void main(String[] args) {
+		System.out.println("Mostrando PRIMER mensaje en pantalla");		
+		System.out.println("Mostrando SEGUNDO mensaje en pantalla");		
+
+	}
+
+}
