2586d9045ae06da59a5dc7883342e21cb527ba04
Bilal <Bilal@AX15>
2024-04-15 18:22:05 +0200

    segundo commit para rama1.

--- Proyecto2/src/paqueteRama1/rama1.java
+++ Proyecto2/src/paqueteRama1/rama1.java
@@ -4,7 +4,10 @@ public class rama1 {
 
 	public static void main(String[] args) {
 		System.out.println("Mostrando PRIMER mensaje en pantalla");		
-		System.out.println("Mostrando SEGUNDO mensaje en pantalla");		
+		System.out.println("Mostrando SEGUNDO mensaje en pantalla");	
+		System.out.println("Mostrando TERCER mensaje en pantalla");		
+		System.out.println("Mostrando CUARTO mensaje en pantalla");		
+
 
 	}
 
